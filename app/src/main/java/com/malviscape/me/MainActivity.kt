3abf2c2397e64612ed00208285e4f799d0fd8956
Gagan Malvi <malvi@aospa.co>
2021-04-29 17:50:07 +0530

    Me: [FEATURE] Add fab, load image via Glide

--- app/src/main/java/com/malviscape/me/MainActivity.kt
+++ app/src/main/java/com/malviscape/me/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.malviscape.me
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.ImageViewCompat
+import com.bumptech.glide.Glide
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageview = findViewById<ImageView>(R.id.imageview)
+        val image = "https://github.com/gaganmalvi/gaganmalvi/raw/master/nyan.gif"
+        Glide.with(this)
+             .load(image)
+             .into(imageview)
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val url = "http://github.com/gaganmalvi"
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        }
     }
 }
